f5d92a7a7fb5255b23689a7e3d98f7ea67f5dd22
Matthew Mehrtens <12023414+mcmehrtens@users.noreply.github.com>
2025-05-29 16:07:19 -0500

    added parameters and documentation

--- src/Integrator/Agglomeration.cpp
+++ src/Integrator/Agglomeration.cpp
@@ -4,5 +4,15 @@
 namespace Integrator
 {
 void
-Agglomeration::Parse(Agglomeration &value, IO::ParmParse &pp) {}
+Agglomeration::Parse(Agglomeration &value, IO::ParmParse &pp)
+{
+    // Chemical potential
+    pp.query_default("gamma", value.gamma, 0.0005);
+    // Surface tension
+    pp.query_default("kappa", value.kappa, 0.001);
+    // Agglomeration mobility
+    pp.query_default("L0", value.L0, 0.5);
+    // Agglomeration mobility exponent
+    pp.query_default("n", value.n, 2.0);
+}
 }
