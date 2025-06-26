0d0a62c86e4eb21ec5e9e060ac0b65b3b2ae67cc
Matthew Mehrtens <12023414+mcmehrtens@users.noreply.github.com>
2025-06-26 14:28:41 -0500

    refactor variable names and fix the math

--- src/Integrator/Agglomeration.cpp
+++ src/Integrator/Agglomeration.cpp
@@ -35,13 +35,13 @@ Agglomeration::Parse(Agglomeration &value, IO::ParmParse &pp)
     pp.queryclass<Flame>("flame", &value);
 
     // Diffuse interface length of the agglomerate phase
-    pp.query_default("eps", value.agglom.eps, 1e-7);
+    pp.query_default("epsilon", value.agglom.epsilon, 1e-7);
     // Chemical potential of the agglomerating material
     pp.query_default("gamma", value.agglom.gamma, 0.0005);
     // Surface tension of the agglomerating material
     pp.query_default("kappa", value.agglom.kappa, 1.0);
     // Agglomeration mobility
-    pp.query_default("L0", value.agglom.L0, 1.0);
+    pp.query_default("L_0", value.agglom.L_0, 1.0);
     // Agglomeration mobility exponent
     pp.query_default("n", value.agglom.n, 1.0);
 
@@ -49,29 +49,29 @@ Agglomeration::Parse(Agglomeration &value, IO::ParmParse &pp)
     pp.query_default("refinement_threshold", value.refinement_threshold, 1e100);
 
     // Initial conditions for agglomerate order parameter
-    pp.select_default<IC::Constant, IC::Expression, IC::BMP, IC::PNG, IC::Random, IC::BetaDistribution>("alpha_agglom.ic", value.ic_alpha_agglom, value.geom);
+    pp.select_default<IC::Constant, IC::Expression, IC::BMP, IC::PNG, IC::Random, IC::BetaDistribution>("alpha.ic", value.agglom.alpha_ic, value.geom);
     // Boundary conditions for agglomerate order parameter
-    pp.select_default<BC::Constant>("alpha_agglom.bc", value.bc_alpha_agglom, 1);
+    pp.select_default<BC::Constant>("alpha.bc", value.agglom.alpha_bc, 1);
 
-    value.RegisterNewFab(value.alphaold_agglom_mf, value.bc_alpha_agglom, 1, 1, "alpha_agglom_old", false);
-    value.RegisterNewFab(value.alpha_agglom_mf, value.bc_alpha_agglom, 1, 1, "alpha_agglom", true);
-    value.RegisterNewFab(value.free_energy_agglom_derivative_mf, value.bc_alpha_agglom, 1, 1, "free_energy_agglom_derivative", true);
+    value.RegisterNewFab(value.agglom.alpha_old, value.agglom.alpha_bc, 1, 1, "agglom.alpha_old", false);
+    value.RegisterNewFab(value.agglom.alpha, value.agglom.alpha_bc, 1, 1, "agglom.alpha", true);
+    value.RegisterNewFab(value.agglom.free_energy_derivative, value.agglom.alpha_bc, 1, 1, "agglom.free_energy_derivative", true);
 };
 
 void
 Agglomeration::Initialize(int lev)
 {
     Flame::Initialize(lev);
-    alphaold_agglom_mf[lev]->setVal(0.0);
-    ic_alpha_agglom->Initialize(lev, alpha_agglom_mf);
-    free_energy_agglom_derivative_mf[lev]->setVal(0.0);
+    agglom.alpha_old[lev]->setVal(0.0);
+    agglom.alpha_ic->Initialize(lev, agglom.alpha);
+    agglom.free_energy_derivative[lev]->setVal(0.0);
 
-    int nComp = alpha_agglom_mf[lev]->nComp();
-    int nGrow = alpha_agglom_mf[lev]->nGrow();
-    MultiFab cell_based_phi(alpha_agglom_mf[lev]->boxArray(), alpha_agglom_mf[lev]->DistributionMap(), nComp, nGrow);
+    int nComp = agglom.alpha[lev]->nComp();
+    int nGrow = agglom.alpha[lev]->nGrow();
+    MultiFab cell_based_phi(agglom.alpha[lev]->boxArray(), agglom.alpha[lev]->DistributionMap(), nComp, nGrow);
     average_node_to_cellcenter(cell_based_phi, 0, *phi_mf[lev], 0, nComp, nGrow);
 
-    scaleByComplement(*alpha_agglom_mf[lev], cell_based_phi, 0, 0, nComp, nGrow);
+    scaleByComplement(*agglom.alpha[lev], cell_based_phi, 0, 0, nComp, nGrow);
 }
 
 void
@@ -97,33 +97,34 @@ void
 Agglomeration::Advance(int lev, Set::Scalar time, Set::Scalar dt)
 {
     Flame::Advance(lev, time, dt);
-    std::swap(alphaold_agglom_mf[lev], alpha_agglom_mf[lev]);
-    const Set::Scalar *DX = geom[lev].CellSize();
-    for (amrex::MFIter mfi(*alpha_agglom_mf[lev], true); mfi.isValid(); ++mfi)
+    std::swap(agglom.alpha_old[lev], agglom.alpha[lev]);
+    const Set::Scalar *dx = geom[lev].CellSize();
+    for (amrex::MFIter mfi(*agglom.alpha[lev], true); mfi.isValid(); ++mfi)
     {
         const amrex::Box &bx = mfi.tilebox();
-        Set::Patch<const Set::Scalar> alphaold_agglom = alphaold_agglom_mf.Patch(lev, mfi);
-        Set::Patch<Set::Scalar> alpha_agglom = alpha_agglom_mf.Patch(lev, mfi);
-        Set::Patch<Set::Scalar> free_energy_agglom_derivative = free_energy_agglom_derivative_mf.Patch(lev, mfi);
+        Set::Patch<const Set::Scalar> alpha_old = agglom.alpha_old.Patch(lev, mfi);
+        Set::Patch<Set::Scalar> alpha = agglom.alpha.Patch(lev, mfi);
+        Set::Patch<Set::Scalar> free_energy_derivative = agglom.free_energy_derivative.Patch(lev, mfi);
         Set::Patch<Set::Scalar> eta = eta_mf.Patch(lev, mfi);
 
         amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
-            Set::Scalar laplacian_alpha = Numeric::Laplacian(alphaold_agglom, i, j, k, 0, DX);
+            // calculate the Laplacian of alpha
+            Set::Scalar laplacian_alpha = Numeric::Laplacian(alpha_old, i, j, k, 0, dx);
 
             // calculate the variational derivative
-            free_energy_agglom_derivative(i, j, k) = 2 * agglom.eps * agglom.gamma * alphaold_agglom(i, j, k) * (1 - alphaold_agglom(i, j, k)) * (1 - 2 * alphaold_agglom(i, j, k)) - agglom.kappa / agglom.eps * laplacian_alpha;
+            free_energy_derivative(i, j, k) = 2 * agglom.gamma / agglom.epsilon * alpha_old(i, j, k) * (1 - alpha_old(i, j, k)) * (1 - 2 * alpha_old(i, j, k)) - agglom.epsilon * agglom.kappa * laplacian_alpha;
         });
 
         amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
             // calculate the Laplacian of the variational derivative
-            Set::Scalar laplacian = Numeric::Laplacian(free_energy_agglom_derivative, i, j, k, 0, DX);
+            Set::Scalar free_energy_laplacian = Numeric::Laplacian(free_energy_derivative, i, j, k, 0, dx);
 
             // calculate effective mobility L
-            Set::Scalar L = agglom.L0 * std::pow(1 - eta(i, j, k), agglom.n);
+            Set::Scalar L = agglom.L_0 * std::pow(1 - eta(i, j, k), agglom.n);
 
             // Cahn-Hilliard equation
-            alpha_agglom(i, j, k) = alphaold_agglom(i, j, k) + dt * L * laplacian;
-            alpha_agglom(i, j, k) = amrex::Clamp(alpha_agglom(i, j, k), small, 1.0);
+            alpha(i, j, k) = alpha_old(i, j, k) + dt * L * free_energy_laplacian;
+            alpha(i, j, k) = amrex::Clamp(alpha(i, j, k), small, 1.0);
         });
     }
 }
@@ -133,17 +134,17 @@ Agglomeration::TagCellsForRefinement(int lev, amrex::TagBoxArray &a_tags, Set::S
 {
     Flame::TagCellsForRefinement(lev, a_tags, time, ngrow);
 
-    const Set::Vector DX(geom[lev].CellSize());
-    Set::Scalar dr = DX.lpNorm<2>();
+    const Set::Vector dx(geom[lev].CellSize());
+    Set::Scalar dr = dx.lpNorm<2>();
 
-    for (amrex::MFIter mfi(*alpha_agglom_mf[lev], amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi)
+    for (amrex::MFIter mfi(*agglom.alpha[lev], amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi)
     {
         const amrex::Box &bx = mfi.tilebox();
         Set::Patch<char> tags = a_tags.array(mfi);
-        Set::Patch<const Set::Scalar> alpha_agglom = alpha_agglom_mf.Patch(lev, mfi);
+        Set::Patch<const Set::Scalar> alpha = agglom.alpha.Patch(lev, mfi);
 
         amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
-            Set::Vector grad = Numeric::Gradient(alpha_agglom, i, j, k, 0, DX.data());
+            Set::Vector grad = Numeric::Gradient(alpha, i, j, k, 0, dx.data());
             if (grad.lpNorm<2>() * dr > refinement_threshold)
                 tags(i, j, k) = amrex::TagBox::SET;
         });
